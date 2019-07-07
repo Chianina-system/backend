ab42a5dfd063a0ef353bb02bad6b4afaf9f44694
wgh <392235259@qq.com>
2019-07-07 18:52:36 +0800

    recover the main function

--- src/engine.cpp
+++ src/engine.cpp
@@ -14,7 +14,22 @@ using namespace std;
 
 
 int main() {
-    system("../lib/eclat -tm ../lib/file/test ../lib/file/out");
-    cout<<123<<endl;
-	return 0;
+    CFG *cfg = new CFG_map();
+    GraphStore *graphstore = new NaiveGraphStore();
+    Singletons * singletons = new Singletons();
+    Grammar *grammar = new Grammar();
+
+//	Partition *partition = nullptr;
+//	cout << cfg << endl;
+
+    CFGCompute::load("/home/zqzuo/Desktop/inlined/final", "/home/zqzuo/Desktop/inlined/id_stmt_info.txt", cfg,
+                     "/home/zqzuo/Desktop/inlined/var_singleton_info.txt", singletons, graphstore, "/home/zqzuo/Desktop/inlined/rules_pointsto.txt", grammar);
+    CFGCompute::do_worklist_synchronous(cfg, graphstore, grammar, singletons);
+//	CFGCompute_asyn::do_worklist_asynchronous(cfg, graphstore, grammar, singletons);
+
+    delete cfg;
+    delete graphstore;
+    delete grammar;
+
+//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 }
