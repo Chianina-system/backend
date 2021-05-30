9b14489860b12d89cb3ca755459fff2ad6dafc3c
sherrysun0613 <30958110+sherrysun0613@users.noreply.github.com>
2021-05-31 00:00:49 +0800

    Update cfg_node.h

--- src/comp/cfg_node.h
+++ src/comp/cfg_node.h
@@ -24,20 +24,13 @@
 #include "stmt/stmt_calleefptr.h"
 #include "graphstore/buffer.h"
 
-//内存中IR信息
+
 using namespace std;
 
 class CFGNode{
 
-	friend std::ostream & operator<<(std::ostream & strm, const CFGNode& cfgNode) {
-		strm << cfgNode.getOutPointer();
-//		if(cfgNode.getStmt()){
-//			strm << cfgNode.getStmt()->toString();
-//		}
-		return strm;
-	}
-
 public:
+
     CFGNode(){
     	id = -1;
     	//len1= len;
@@ -51,200 +44,16 @@ public:
     	id = _id;
     }
 
-//    CFGNode(std::string& line){
-//		std::stringstream stream(line);
-//		std::string stmt_id, type;
-//		stream >> stmt_id >> type;
-//
-//		this->id = atoi(stmt_id.c_str());
-//		if (type == "assign") {
-//			stmt = new AssignStmt(stream);
-//		}
-//		else if (type == "load") {
-//			stmt = new LoadStmt(stream);
-//		}
-//		else if (type == "store") {
-//			stmt = new StoreStmt(stream);
-//		}
-//		else if (type == "alloca") {
-//			stmt = new AllocStmt(stream);
-//		}
-//		else if (type == "phi") {
-//			stmt = new PhiStmt(stream);
-//		}
-//		else if (type == "call") {
-//			stmt = new CallStmt(stream);
-//		}
-//		else if (type == "return") {
-//			stmt = new ReturnStmt(stream);
-//		}
-//		else if (type == "ret") {
-//			stmt = new RetStmt();
-//		}
-//		else if (type == "block") {
-//			stmt = new SkipStmt();
-//		}
-//		else if(type == "callfptr"){
-//			stmt = new CallfptrStmt(stream);
-//		}
-//		else if(type == "calleefptr"){
-//			stmt = new CalleefptrStmt(stream);
-//		}
-//		else {
-//			cout << "wrong stmt type!!!" << endl;
-//			exit(EXIT_FAILURE);
-//		}
-//	}
-    CFGNode(std::string& line) {
-	    std::stringstream stream(line);
-		std::string stmt_id;
-		stream >> stmt_id;
-        this->id = atoi(stmt_id.c_str());
-        std::string arg;
-        while(stream >> arg) {
-            int address = 0;
-            //address = hexToDec(arg);
-            sscanf(arg.c_str(), "%x", &address);
-            //cout << address << endl;
-            content.push_back(address);
-        }
-	}
+    virtual ~CFGNode(){   };
 
-    ~CFGNode(){
-//    	if(stmt){
-//			delete stmt;
-//    	}
-    }
+    virtual size_t get_size_bytes() const = 0;
 
-    int getHexValue(char a) {
+    virtual void write_to_buf(Buffer& buf) = 0;
 
-        int tmp = 0;
-        if (a >= '0'&&a <= '9')
-            tmp = a - '0';
-        else {
-            switch (a)
-            {
-                case 'a':
-                case 'A':
-                    tmp = 10;
-                    break;
-                case 'b':
-                case 'B':
-                    tmp = 11;
-                    break;
-                case 'c':
-                case 'C':
-                    tmp = 12;
-                    break;
-                case 'd':
-                case 'D':
-                    tmp = 13;
-                    break;
-                case 'e':
-                case 'E':
-                    tmp = 14;
-                    break;
-                case 'f':
-                case 'F':
-                    tmp = 15;
-                    break;
-                default:
-                    tmp = 0;
-                    break;
-            }
-
-        }
-
-        return tmp;
-    }
-    int  hexToDec(std::string addr) {
-        string addrH = addr.substr(2); // 以0x40063b为例，截取后面具体的16进制地址
-        int sum = 0;
-        int len = addrH.length();
-        int pow = 1;
-        for (int i = 0; i <len; i++) {
-            sum += getHexValue(addrH[len - i - 1])*pow;
-            pow = pow * 16;
-        }
+    virtual void read_from_buf(char* buf, size_t bufsize) = 0;
 
 
-        return sum;  //行内偏移  sum = sum / 64;
-        //组号 sum = sum % 128;
-    }
-
-    size_t get_size_bytes() const {
-//    	return sizeof(PEGraph_Pointer) + sizeof(TYPE) + stmt->get_size_bytes();
-        return sizeof(PEGraph_Pointer) + sizeof(std::vector<IR>::size_type) + sizeof(IR) * content.size();
-    }
-
-    void write_to_buf(Buffer& buf){
-    	size_t s = get_size_bytes();
-    	memcpy(buf.getData() + buf.getSize(), (char*)& s, sizeof(size_t));
-    	buf.add_size_by(sizeof(size_t));
-
-    	memcpy(buf.getData() + buf.getSize(), (char*)& id, sizeof(PEGraph_Pointer));
-    	buf.add_size_by(sizeof(PEGraph_Pointer));
-
-    	std::vector<IR>::size_type size = content.size();
-        memcpy(buf.getData() + buf.getSize(), (char*)& size, sizeof(std::vector<IR>::size_type));
-        buf.add_size_by(sizeof(std::vector<IR>::size_type));
-
-
-        for (int i = 0; i < content.size(); ++i) {
-            IR ir = content[i];
-            memcpy(buf.getData() + buf.getSize(), (char*)& ir, sizeof(IR));
-            buf.add_size_by(sizeof(IR));
-        }
-
-
-//    	TYPE t = stmt->getType();
-//    	memcpy(buf.getData() + buf.getSize(), (char*)& t, sizeof(TYPE));
-//    	buf.add_size_by(sizeof(TYPE));
-//    	stmt->write_to_buf(buf);
-    }
-
-    void read_from_buf(char* buf, size_t bufsize){
-    	size_t offset = 0;
-    	id = *((PEGraph_Pointer*)(buf));
-    	offset += sizeof(PEGraph_Pointer);
-    	std::vector<IR>::size_type size = *((std::vector<IR>::size_type*)(buf + offset));
-        offset += sizeof(std::vector<IR>::size_type);
-        for (int i = 0; i < size; i++) {
-            IR ir = *((IR*)(buf + offset));
-            content.push_back(ir);
-            offset += sizeof(IR);
-        }
-//    	TYPE type = *((TYPE*)(buf + offset));
-//    	offset += sizeof(TYPE);
-//
-//    	switch(type){
-//    	case TYPE::Assign: stmt = new AssignStmt(); break;
-//    	case TYPE::Load: stmt = new LoadStmt(); break;
-//    	case TYPE::Store: stmt = new StoreStmt(); break;
-//    	case TYPE::Alloca: stmt = new AllocStmt(); break;
-//    	case TYPE::Phi: stmt = new PhiStmt(); break;
-//    	case TYPE::Call: stmt = new CallStmt(); break;
-//    	case TYPE::Return: stmt = new ReturnStmt(); break;
-//    	case TYPE::Ret: stmt = new RetStmt(); break;
-//    	case TYPE::Skip: stmt = new SkipStmt(); break;
-//    	case TYPE::Callfptr: stmt = new CallfptrStmt(); break;
-//    	case TYPE::Calleefptr: stmt = new CalleefptrStmt(); break;
-//    	default:
-//    		cout << "wrong stmt type!!!" << endl;
-//    		exit(EXIT_FAILURE);
-//    	}
-//
-//    	stmt->read_from_buf(buf, offset, bufsize);
-    }
-
-    inline Stmt* getStmt() const {
-        //return stmt;
-        return nullptr;
-    }
-
-//	inline PEGraph_Pointer getInPointer() const {
-//		return in_pointer;
-//	}
+    virtual inline Stmt* getStmt() const  = 0;
 
     inline PEGraph_Pointer getOutPointer() const {
         return id;
@@ -258,37 +67,13 @@ public:
         id = cfgNodeId;
     }
 
-    inline void setID(int id) {
-        this->id = id;
-    }
 
-    inline void addIR(IR inst) {
-        content.push_back(inst);
-
-    }
-
-    inline std::vector<int> getAllContent() {
-        return content;
-    }
-
-
-
-
-
-
-private:
+protected:
     PEGraph_Pointer id;
-
-    std::vector<IR> content;
-    // int len = 0;
-    //std:: int *content;
-
-    //Stmt* stmt = nullptr;
-
-
 
 };
 
 
-
 #endif /* COMP_CFG_NODE_H_ */
+
+
